ce82b00ba16af6a4a7d6e52c7fa9245d9a430676
just_a_mango <horace.hoff@gmail.com>
2024-03-15 19:59:06 +0100

    /

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod process;
+mod util;
+mod types;
+
+use std::fs::File;
+use std::io::{BufReader, prelude::*};
+
+
+fn main() {
+    let file = File::open("foo.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut i = 1;
+    for line in reader.lines() {
+        process::process_line(line.unwrap(), i);
+        i += 1;
+    }
+}
