0b09ba3d34ab2820c2b2e2741181b1e62d4fa1a9
just_a_mango <horace.hoff@gmail.com>
2024-10-12 15:21:06 +0200

    /

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
-mod process;
-mod util;
-mod types;
-mod operations;
-
-use std::fs::File;
-use std::io::{BufReader, prelude::*};
-
+use std::fs;
+use regex::Regex;
 
 fn main() {
-    let file = File::open("foo.txt").unwrap();
-    let reader = BufReader::new(file);
-    
-    let mut i = 1;
-    for line in reader.lines() {
-        process::process_line(line.unwrap(), i);
-        i += 1;
+    let mut functions: Vec<(&str, &str, &str)> = vec![];
+
+
+    let mut content = fs::read_to_string("foo.txt").unwrap();
+
+
+    // PRE_PROCESSING
+
+
+    // Remove comments
+    content = Regex::new(r"//.*").unwrap().replace_all(&content, "").parse().unwrap();
+    println!("{:?}", content);
+
+
+
+    // Register functions
+    let function_regex = Regex::new(r"func (.*)\((.*)\)\s*\{((?s:.)*)}").unwrap();
+    let function_results: Vec<_> = function_regex.find_iter(&content).collect();
+    for func_match in function_results.iter() {
+        let function = function_regex.captures(func_match.as_str()).unwrap();
+        functions.push((function.get(1).unwrap().as_str(), function.get(2).unwrap().as_str(), function.get(3).unwrap().as_str()));
     }
+
+
+
+    // Register main function
+    let main_regex = Regex::new(r"main\s*\{((?s:.)*)}").unwrap();
+    let main_block_match = main_regex.captures(&content).unwrap();
+    let main_block = main_block_match.get(1).unwrap().as_str();
+    println!("{:?}", functions);
+
+
+
 }
