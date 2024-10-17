162a5b5f511a3935b062b7e330c387e0604d8451
just_a_mango <horace.hoff@gmail.com>
2024-10-17 20:33:46 +0200

    /

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ use crate::functions::parse_functions;
 
 fn main() {
     let content = fs::read_to_string("foo.txt").unwrap();
-    
+
     let functions: Vec<(&str, &str, Vec<&str>)> = parse_functions(&content);
-    
-    
+
     println!("{:?}", functions);
+
+
+    // RUN MAIN FUNCTION
 }
