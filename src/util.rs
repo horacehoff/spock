44a832fbaf17f195b9884aeeddb3c55f02508e43
just_a_mango <horace.hoff@gmail.com>
2024-10-17 20:38:04 +0200

    /

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,4 @@
-use colored::Colorize;
-
 pub fn error(message: &str, tip: &str) {
-    eprintln!("--------------\n{}\n{}\n{}\n{}\n--------------","COMPUTE ERROR:".red(), message, "POSSIBLE SOLUTION:".blue(), tip);
+    eprintln!("--------------\n{}\n{}\n{}\n{}\n--------------","\u{001b}[31mCOMPUTE ERROR:\u{001b}[0m", message, "\u{001b}[34mPOSSIBLE SOLUTION:\u{001b}[0m", tip);
     std::process::exit(1);
 }
