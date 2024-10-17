76da25921df2f5b1ce5629b764dd0aaaffeb2ea9
just_a_mango <horace.hoff@gmail.com>
2024-10-17 19:12:16 +0200

    /

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,7 @@ use fancy_regex::Regex;
 
 
 fn main() {
-    let mut functions: Vec<(&str, &str, &str)> = vec![];
-
-
+    let mut functions: Vec<(&str, &str, Vec<&str>)> = vec![];
     let mut content = fs::read_to_string("foo.txt").unwrap();
 
 
@@ -26,20 +24,27 @@ fn main() {
     // Register functions
     let function_regex = Regex::new(r"(?ms)^func\s+(\w+)\s*\((.*?)\)\s*\{(.*?)}(?=((\s*func|\z)))").unwrap();
     let function_results: Vec<_> = function_regex.captures_iter(&content).collect();
+
+    let line_regex = Regex::new(r"(?m)^(.*|})(?:;|\s*{|(?<=}))");
+
     for func_match in function_results.iter() {
         let function = func_match.as_ref().unwrap();
-        functions.push((function.get(1).unwrap().as_str(), function.get(2).unwrap().as_str(), function.get(3).unwrap().as_str()));
+
+
+        // Separate lines by commas - not working
+        let line_results: Vec<_> = function_regex.captures_iter(function.get(3).unwrap().as_str()).map(|line| line.as_ref().unwrap().get(1).unwrap().as_str()).collect();
+        println!("{:?}", line_results);
+        
+        functions.push((function.get(1).unwrap().as_str(), function.get(2).unwrap().as_str(), line_results));
     }
 
     // Cache functions
     let mut functions_file = fs::File::create(".functions").unwrap();
     functions_file.write_all(format!("{:?}", functions).as_bytes()).unwrap();
 
-    if functions.clone().into_iter().filter(|function| function.0 == "main").collect::<Vec<(&str, &str, &str)>>().len() == 0 {
+    if functions.clone().into_iter().filter(|function| function.0 == "main").collect::<Vec<(&str, &str, Vec<&str>)>>().len() == 0 {
         error("No main function", "Add 'func main() {}' to your file");
     }
-
-
 
 
     println!("{:?}", functions);
