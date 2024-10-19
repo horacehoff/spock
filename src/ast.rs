419baae2197a84f7704b71c97b9a954cf941dfa7
Just_a_Mango <horace.hoff@gmail.com>
2024-10-19 20:37:49 +0200

    /

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,6 @@
+use pest::iterators::Pair;
+use crate::Rule;
+
 #[derive(Debug)]
 pub enum Expr {
     Integer(i64),
@@ -5,7 +8,7 @@ pub enum Expr {
     String(String),
     Identifier(String),
     BinaryOp(BasicOperator, Box<Expr>),
-    Priority(Box<Expr>),
+    Priority(Box<Vec<Expr>>),
     Operation(BasicOperator)
 }
 
@@ -28,4 +31,66 @@ pub enum BasicOperator {
 pub struct VariableDeclaration {
     variable: String,
     value: Expr,
+}
+
+pub fn build_ast(pair: Pair<Rule>, indent: usize) -> Vec<Expr> {
+    let rule = format!("{:?}", pair.as_rule());
+    let span = pair.as_span();
+    let text = span.as_str();
+    let mut output: Vec<Expr> = vec![];
+    let mut recursive = true;
+
+    match pair.as_rule() {
+        Rule::integer => {
+            output.push(Expr::Integer(pair.as_str().parse::<i64>().unwrap()))
+        },
+        Rule::float => {
+            output.push(Expr::Float(pair.as_str().parse::<f32>().unwrap()))
+        },
+        Rule::string => {
+            output.push(Expr::String(pair.as_str().parse().unwrap()))
+        },
+        Rule::priority => {
+            recursive = false;
+            let mut priority_calc: Vec<Expr> = vec![];
+            for priority_pair in pair.clone().into_inner() {
+                priority_calc.append(&mut build_ast(priority_pair, 0));
+            }
+            output.push(Expr::Priority(Box::from(priority_calc)))
+        }
+        Rule::ops => {
+            match pair.as_str() {
+                "+" => {
+                    output.push(Expr::Operation(BasicOperator::Add))
+                },
+                "-" => {
+                    output.push(Expr::Operation(BasicOperator::Sub))
+                },
+                "/" => {
+                    output.push(Expr::Operation(BasicOperator::Divide))
+                },
+                "*" => {
+                    output.push(Expr::Operation(BasicOperator::Multiply))
+                }
+                _ => {}
+            }
+        }
+        _ => {}
+    }
+    // Print the current node with indentation
+    println!(
+        "{}{}: \"{}\"",
+        "  ".repeat(indent),  // Indentation based on depth
+        rule,
+        text
+    );
+
+    if recursive {
+        // Recursively process the children
+        for inner_pair in pair.into_inner() {
+            output.append(&mut build_ast(inner_pair, indent + 1));  // Increase indent for child nodes
+        }
+    }
+
+    output
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,8 @@ mod functions;
 mod ast;
 
 use std::fs;
-use pest::iterators::Pair;
 use pest::Parser;
 use pest_derive::Parser;
-use sha2::digest::typenum::op;
-use crate::ast::{BasicOperator, Expr, VariableDeclaration};
-use crate::ast::Expr::Float;
 use crate::functions::parse_functions;
 
 
@@ -53,58 +49,6 @@ WHITESPACE = _{ " " | "\t" | "\n" }
 struct LineParser;
 
 
-
-fn visualize_parse_tree(pair: Pair<Rule>, indent: usize) -> Vec<Expr> {
-    let rule = format!("{:?}", pair.as_rule());
-    let span = pair.as_span();
-    let text = span.as_str();
-    let mut output: Vec<Expr> = vec![];
-
-    match pair.as_rule() {
-        Rule::integer => {
-            output.push(Expr::Integer(pair.as_str().parse::<i64>().unwrap()))
-        },
-        Rule::float => {
-            output.push(Expr::Float(pair.as_str().parse::<f32>().unwrap()))
-        },
-        Rule::string => {
-            output.push(Expr::String(pair.as_str().parse().unwrap()))
-        }
-        Rule::ops => {
-            match pair.as_str() {
-                "+" => {
-                    output.push(Expr::Operation(BasicOperator::Add))
-                },
-                "-" => {
-                    output.push(Expr::Operation(BasicOperator::Sub))
-                },
-                "/" => {
-                    output.push(Expr::Operation(BasicOperator::Divide))
-                },
-                "*" => {
-                    output.push(Expr::Operation(BasicOperator::Multiply))
-                }
-                _ => {}
-            }
-        }
-        _ => {}
-    }
-    // Print the current node with indentation
-    println!(
-        "{}{}: \"{}\"",
-        "  ".repeat(indent),  // Indentation based on depth
-        rule,
-        text
-    );
-
-    // Recursively process the children
-    for inner_pair in pair.into_inner() {
-        output.append(&mut visualize_parse_tree(inner_pair, indent + 1));  // Increase indent for child nodes
-    }
-
-    output
-}
-
 fn main() {
     let content = fs::read_to_string("foo.txt").unwrap();
 
@@ -115,7 +59,7 @@ fn main() {
     println!("{:?}", main_lines);
     for line in main_lines {
         for pair in LineParser::parse(Rule::expression, line).unwrap() {
-            println!("{:?}", visualize_parse_tree(pair, 0));
+            println!("{:?}", ast::build_ast(pair, 0));
         }
 
     }
