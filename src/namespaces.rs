67664f8b41164cbfa878d65cd0e9992d29dcb44c
just_a_mango <horace.hoff@gmail.com>
2024-11-06 21:39:36 +0100

    /

--- src/namespaces.rs
+++ src/namespaces.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::Write;
 use std::process::exit;
 use crate::{assert_args_number, error_msg, get_printable_form};
 use crate::parser::Expr;
@@ -27,7 +28,42 @@ pub fn namespace_functions(x: Vec<String>, y: String, args: Vec<Expr>) -> (Expr,
                 error(&format!("Invalid file name: {:?}", get_printable_form(args[0].clone())),"");
                 (Expr::Null, true)
             }
-        } else {
+        } else if y == "write" {
+            assert_args_number!(y, args.len(), 2);
+            if let Expr::String(filename) = args[0].clone() {
+                if let Expr::String(filecontent) = args[1].clone() {
+                    let mut f = fs::OpenOptions::new().write(true).truncate(true).open(&filename).expect(error_msg!(format!("Failed to open {}", filename)));
+                    f.write_all(filecontent.as_ref()).expect(error_msg!(format!("Failed to write {} to {}", filecontent, filename)));
+                    f.flush().unwrap();
+                    (Expr::Null, true)
+
+                } else {
+                    error(&format!("Invalid file content: {:?}", get_printable_form(args[1].clone())),"");
+                    (Expr::Null, true)
+                }
+            } else {
+                error(&format!("Invalid file name: {:?}", get_printable_form(args[0].clone())),"");
+                (Expr::Null, true)
+            }
+        } else if y == "append" {
+            assert_args_number!(y, args.len(), 2);
+            if let Expr::String(filename) = args[0].clone() {
+                if let Expr::String(filecontent) = args[1].clone() {
+                    let mut f = fs::OpenOptions::new().write(true).append(true).open(&filename).expect(error_msg!(format!("Failed to open {}", filename)));
+                    f.write_all(filecontent.as_ref()).expect(error_msg!(format!("Failed to append {} to {}", filecontent, filename)));
+                    f.flush().unwrap();
+                    (Expr::Null, true)
+
+                } else {
+                    error(&format!("Invalid file content: {:?}", get_printable_form(args[1].clone())),"");
+                    (Expr::Null, true)
+                }
+            } else {
+                error(&format!("Invalid file name: {:?}", get_printable_form(args[0].clone())),"");
+                (Expr::Null, true)
+            }
+        }
+        else {
             (Expr::Null, false)
         }
     }
