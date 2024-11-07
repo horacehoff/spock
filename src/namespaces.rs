be00601c44c3646af3f3c3bd9504bc2b1ea91e5b
just_a_mango <horace.hoff@gmail.com>
2024-11-07 20:27:52 +0100

    /

--- src/namespaces.rs
+++ src/namespaces.rs
@@ -29,50 +29,6 @@ pub fn namespace_functions(x: Vec<String>, y: String, args: Vec<Expr>) -> (Expr,
                 (Expr::Null, true)
             }
         }
-        else if y == "read" {
-            assert_args_number!(y, args.len(), 1);
-            if let Expr::String(filename) = args[0].clone() {
-                let filecontent = fs::read_to_string(&filename).expect(error_msg!(format!("Failed to read {}", filename)));
-                (Expr::String(filecontent), true)
-            } else {
-                error(&format!("Invalid file name: {:?}", get_printable_form(args[0].clone())),"");
-                (Expr::Null, true)
-            }
-        } else if y == "write" {
-            assert_args_number!(y, args.len(), 2);
-            if let Expr::String(filename) = args[0].clone() {
-                if let Expr::String(filecontent) = args[1].clone() {
-                    let mut f = fs::OpenOptions::new().write(true).truncate(true).open(&filename).expect(error_msg!(format!("Failed to open {}", filename)));
-                    f.write_all(filecontent.as_ref()).expect(error_msg!(format!("Failed to write {} to {}", filecontent, filename)));
-                    f.flush().unwrap();
-                    (Expr::Null, true)
-
-                } else {
-                    error(&format!("Invalid file content: {:?}", get_printable_form(args[1].clone())),"");
-                    (Expr::Null, true)
-                }
-            } else {
-                error(&format!("Invalid file name: {:?}", get_printable_form(args[0].clone())),"");
-                (Expr::Null, true)
-            }
-        } else if y == "append" {
-            assert_args_number!(y, args.len(), 2);
-            if let Expr::String(filename) = args[0].clone() {
-                if let Expr::String(filecontent) = args[1].clone() {
-                    let mut f = fs::OpenOptions::new().write(true).append(true).open(&filename).expect(error_msg!(format!("Failed to open {}", filename)));
-                    f.write_all(filecontent.as_ref()).expect(error_msg!(format!("Failed to append {} to {}", filecontent, filename)));
-                    f.flush().unwrap();
-                    (Expr::Null, true)
-
-                } else {
-                    error(&format!("Invalid file content: {:?}", get_printable_form(args[1].clone())),"");
-                    (Expr::Null, true)
-                }
-            } else {
-                error(&format!("Invalid file name: {:?}", get_printable_form(args[0].clone())),"");
-                (Expr::Null, true)
-            }
-        }
         else {
             (Expr::Null, false)
         }
