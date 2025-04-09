c098367f7f32e6b884b8f33fdb3f0b48de32b07d
HoraceHoff <horace.hoff@gmail.com>
2025-04-09 17:53:27 +0200

    /

--- src/tests.rs
+++ src/tests.rs
@@ -15,6 +15,14 @@ mod tests {
     }
 
     #[test]
+    fn fibonacci_140() {
+        let instr = vec![Sub(0,5,6), Inf(4,6,7), Cmp(7,6), Add(1,2,3), Mov(2,1), Mov(3,2), Add(4,11,4),Jmp(6, true)];
+        let mut consts = vec![Number(70.0), Number(0.0), Number(1.0), Number(0.0), Number(0.0), Number(1.0), Null, Null, Null, Null, Null, Number(1.0), Null];
+        execute(&instr, &mut consts, 0, &mut HashMap::default());
+        assert_eq!(consts[3], Number(190392490709135.0));
+    }
+
+    #[test]
     fn array_push() {
         let instr = vec![StoreFuncArg(1), ApplyFunc(14, 0, 2)];
         let mut consts = vec![Array(0), Number(7.0), Null];
