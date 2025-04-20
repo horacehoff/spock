459eeafc954f81febb0acfc09424240ec8b47794
just_a_mango <horace.hoff@gmail.com>
2025-04-20 20:17:27 +0200

    /

--- src/optimizations.rs
+++ src/optimizations.rs
@@ -73,33 +73,38 @@ pub fn for_loop_summation(
             if let Expr::Op(base, tgt) = &**x {
                 if tgt.len() == 1 {
                     let (op, dest) = tgt.first().unwrap();
-                    if op == &Opcode::Add {
-                        if **base == Expr::Var(name.to_string()) {
-                            let var_id = v.iter().find(|(w, _)| w == name).unwrap().1;
-                            if let Expr::Num(reps) = **dest {
-                                if let Data::Array(id) = consts[array as usize] {
-                                    let len = arrs[&id].len();
+                    if **base == Expr::Var(name.to_string()) {
+                        let var_id = v.iter().find(|(w, _)| w == name).unwrap().1;
+                        if let Expr::Num(reps) = **dest {
+                            if let Data::Array(id) = consts[array as usize] {
+                                let len = arrs[&id].len();
+                                if op == &Opcode::Add {
                                     consts.push(Data::Number(reps * len as f64));
-                                    output.push(Instr::Mov((consts.len() - 1) as u16, var_id));
-                                    return true;
-                                } else {
-                                    consts.push(Data::Null);
-                                    output.push(Instr::ApplyFunc(
-                                        2,
-                                        array,
-                                        (consts.len() - 1) as u16,
-                                    ));
-                                    consts.push(Data::Number(reps));
-                                    consts.push(Data::Null);
+                                } else if op == &Opcode::Mul {
+                                    consts.push(Data::Number(reps.powi(len as i32)));
+                                }
+                                output.push(Instr::Mov((consts.len() - 1) as u16, var_id));
+                            } else {
+                                consts.push(Data::Null);
+                                output.push(Instr::ApplyFunc(2, array, (consts.len() - 1) as u16));
+                                consts.push(Data::Number(reps));
+                                consts.push(Data::Null);
+                                if op == &Opcode::Add {
                                     output.push(Instr::Mul(
                                         (consts.len() - 3) as u16,
                                         (consts.len() - 2) as u16,
                                         (consts.len() - 1) as u16,
                                     ));
-                                    output.push(Instr::Mov((consts.len() - 1) as u16, var_id));
-                                    return true;
+                                } else if op == &Opcode::Mul {
+                                    output.push(Instr::Pow(
+                                        (consts.len() - 2) as u16,
+                                        (consts.len() - 3) as u16,
+                                        (consts.len() - 1) as u16,
+                                    ));
                                 }
+                                output.push(Instr::Mov((consts.len() - 1) as u16, var_id));
                             }
+                            return true;
                         }
                     }
                 }
