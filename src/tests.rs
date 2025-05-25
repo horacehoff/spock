14a1f5f7819a79abaa5a83a47ca868b21e5f03a6
just_a_mango <horace.hoff@gmail.com>
2025-05-25 20:17:06 +0200

    /

--- src/tests.rs
+++ src/tests.rs
@@ -8,7 +8,7 @@ mod tests {
     #[test]
     fn fib_41() {
         let filename = "TEST_fib_41";
-        let contents = "fn main() { for _ in range(0,200000) { let n = 40; let a=0; let b=1; let c=0; let i=0; while i < n { c = a+b; a = b; b = c; i = i+1; } } }";
+        let contents = "fn main() { for _ in range(0,200000) { let n = 40; let a=0; let b=1; let c=0; let i=0; while i < n { c = a+b; a = b; b = c; i = i+1; } } let x = if 1 == 1 {10} else {20}; if (x == 10) {x += 1;} }";
         let (instructions, mut consts, mut arrays, instr_src) = parse(contents, filename);
         let (func_args_count, call_stack_count) = get_vec_capacity(&instructions);
         execute(
