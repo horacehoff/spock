29c4dc4140fdcc45727f448e22309b3439a7656d
just_a_mango <horace.hoff@gmail.com>
2026-03-28 20:44:26 +0100

    QOL

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use crate::data::Data;
 use crate::display::{format_data, parser_error};
+use crate::instr::Instr;
+use crate::instr::LibFunc;
 use crate::parser::parse;
 use crate::util::error;
 use crate::util::likely;
@@ -8,7 +10,6 @@ use concat_string::concat_string;
 use inline_colorization::*;
 use parser::*;
 use slab::Slab;
-use std::cmp::PartialEq;
 use std::fs::File;
 use std::hint::black_box;
 use std::io::Write;
@@ -21,6 +22,8 @@ mod data;
 mod display;
 #[path = "./parser/functions.rs"]
 mod functions;
+#[path = "./instr.rs"]
+mod instr;
 #[path = "./parser/method_calls.rs"]
 mod method_calls;
 #[path = "./parser/optimizations.rs"]
@@ -31,140 +34,6 @@ mod parser;
 mod type_system;
 #[path = "./util/util.rs"]
 mod util;
-
-#[derive(Debug, Clone, PartialEq, Copy)]
-#[repr(u8)]
-pub enum Instr {
-    Print(u16),
-
-    // LOGIC
-    /// Jumps x instructions forwards
-    Jmp(u16),
-    /// Jumps x instructions backwards
-    JmpBack(u16),
-    /// Cmp(condition_register_id, jump_size) - jumps if false
-    Cmp(u16, u16),
-    InfFloatCmp(u16, u16, u16),
-    InfIntCmp(u16, u16, u16),
-    InfEqFloatCmp(u16, u16, u16),
-    InfEqIntCmp(u16, u16, u16),
-    SupFloatCmp(u16, u16, u16),
-    SupIntCmp(u16, u16, u16),
-    /// Cmp(condition_register_id, jump_size) - jumps if false
-    SupEqFloatCmp(u16, u16, u16),
-    SupEqIntCmp(u16, u16, u16),
-    EqCmp(u16, u16, u16),
-    NotEqCmp(u16, u16, u16),
-    ArrayEqCmp(u16, u16, u16),
-    ArrayNotEqCmp(u16, u16, u16),
-
-    Mov(u16, u16),
-
-    // OPS
-    /// (3) = (1) + (2)
-    AddFloat(u16, u16, u16),
-    AddInt(u16, u16, u16),
-    /// (3) = (1) ∪ (2)
-    AddArray(u16, u16, u16),
-    /// (3) = (1) + (2)
-    AddStr(u16, u16, u16),
-    /// (3) = (1) * (2)
-    MulFloat(u16, u16, u16),
-    MulInt(u16, u16, u16),
-    SubFloat(u16, u16, u16),
-    SubInt(u16, u16, u16),
-    DivFloat(u16, u16, u16),
-    DivInt(u16, u16, u16),
-    ModFloat(u16, u16, u16),
-    ModInt(u16, u16, u16),
-    PowFloat(u16, u16, u16),
-    PowInt(u16, u16, u16),
-    Eq(u16, u16, u16),
-    NotEq(u16, u16, u16),
-    ArrayEq(u16, u16, u16),
-    ArrayNotEq(u16, u16, u16),
-    SupFloat(u16, u16, u16),
-    SupInt(u16, u16, u16),
-    SupEqFloat(u16, u16, u16),
-    SupEqInt(u16, u16, u16),
-    InfFloat(u16, u16, u16),
-    InfInt(u16, u16, u16),
-    InfEqFloat(u16, u16, u16),
-    InfEqInt(u16, u16, u16),
-    BoolAnd(u16, u16, u16),
-    BoolOr(u16, u16, u16),
-    NegFloat(u16, u16),
-    NegInt(u16, u16),
-
-    // General functions
-    Float(u16, u16),
-    Int(u16, u16),
-    Str(u16, u16),
-    Bool(u16, u16),
-    Input(u16, u16),
-    Floor(u16, u16),
-    SqrtFloat(u16, u16),
-    /// start,end,dest
-    Range(u16, u16, u16),
-    // path - dest - create?
-    IoOpen(u16, u16, u16),
-    IoDelete(u16),
-
-    StoreFuncArg(u16),
-    CallLibFunc(LibFunc, u16, u16),
-
-    ArrayMov(u16, u16, u16),
-    // different than ArrayMov => looks into the registers
-    ArrayMod(u16, u16, u16),
-    StrMod(u16, u16, u16),
-    ArrayGet(u16, u16, u16),
-    ArrayStrGet(u16, u16, u16),
-
-    TheAnswer(u16),
-    // array - element
-    Push(u16, u16),
-    // array - index
-    Remove(u16, u16),
-    // array/str - dest
-    Len(u16, u16),
-    // tgt - separator - dest
-    Split(u16, u16, u16),
-
-    /// CallFunc(n,y) - Jumps to the nth instruction, and adds y as a slot to be set by the Return instruction; VoidReturn/Return will jump back to this location
-    CallFunc(u16, u16),
-    /// CallFuncRecursive(n, register_id) - Jumps to the nth instruction, register_id is only used during parsing.
-    CallFuncRecursive(u16, u16),
-    /// Jumps to the instruction right after the last CallFunc encountered by the interpreter
-    VoidReturn,
-    /// Return(n) => returns the data located in register n
-    Return(u16),
-    /// RecursiveReturn(n,function_id) => returns the data located in register n, and restores the function's register state
-    RecursiveReturn(u16, u16),
-    /// SaveFrame(function_location, return_register, function_id)
-    SaveFrame(u16, u16, u16),
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum LibFunc {
-    Uppercase = 0,
-    Lowercase = 1,
-    Contains = 2,
-    Trim = 3,
-    TrimSequence = 4,
-    Index = 5,
-    IsNum = 6,
-    TrimLeft = 7,
-    TrimRight = 8,
-    TrimSequenceLeft = 9,
-    TrimSequenceRight = 10,
-    RIndex = 11,
-    Repeat = 12,
-    Round = 13,
-    Abs = 14,
-    ReadFile = 15,
-    WriteFile = 16,
-    Reverse = 17,
-}
 
 pub type ArrayStorage = Slab<Vec<Data>>;
 
