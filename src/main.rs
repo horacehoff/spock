b0c2ddef340b83d7ef232cc5125fd6f6b58e2b9f
horacehoff <horace.hoff@gmail.com>
2026-03-29 18:32:59 +0200

    Optimization

--- src/main.rs
+++ src/main.rs
@@ -819,6 +819,7 @@ fn main() {
             &fn_registers,
             10,
             150,
+            std::env::args().len() > 3 && std::env::args().nth(3).unwrap() == "--verbose",
         );
     } else {
         let now = Instant::now();
@@ -848,6 +849,7 @@ pub fn benchmark(
     fn_registers: &[Vec<u16>],
     warmup_runs: usize,
     samples_count: usize,
+    verbose: bool,
 ) {
     let mut times_ns: Vec<u128> = Vec::with_capacity(samples_count);
 
@@ -875,54 +877,55 @@ pub fn benchmark(
         ));
         times_ns.push(now.elapsed().as_nanos());
     }
+    if verbose {
+        let mut sorted = times_ns.clone();
+        sorted.sort_unstable();
 
-    let mut sorted = times_ns.clone();
-    sorted.sort_unstable();
+        let min_ns = sorted[0];
+        let max_ns = *sorted.last().unwrap();
+        let median_ns = if samples_count.is_multiple_of(2) {
+            (sorted[samples_count / 2 - 1] + sorted[samples_count / 2]) / 2
+        } else {
+            sorted[samples_count / 2]
+        };
 
-    let min_ns = sorted[0];
-    let max_ns = *sorted.last().unwrap();
-    let median_ns = if samples_count.is_multiple_of(2) {
-        (sorted[samples_count / 2 - 1] + sorted[samples_count / 2]) / 2
-    } else {
-        sorted[samples_count / 2]
-    };
+        let mean_ns = times_ns.iter().sum::<u128>() as f64 / samples_count as f64;
 
-    let mean_ns = times_ns.iter().sum::<u128>() as f64 / samples_count as f64;
+        let variance = times_ns
+            .iter()
+            .map(|&x| (x as f64 - mean_ns).powi(2))
+            .sum::<f64>()
+            / (samples_count as f64);
+        let std_deviation = variance.sqrt();
 
-    let variance = times_ns
-        .iter()
-        .map(|&x| (x as f64 - mean_ns).powi(2))
-        .sum::<f64>()
-        / (samples_count as f64);
-    let std_deviation = variance.sqrt();
+        let std = std_deviation / (samples_count as f64).sqrt();
+        let confidence_interval_margin = 1.96 * std;
 
-    let std = std_deviation / (samples_count as f64).sqrt();
-    let confidence_interval_margin = 1.96 * std;
+        let lower_confidence_interval = mean_ns - confidence_interval_margin;
+        let upper_confidence_interval = mean_ns + confidence_interval_margin;
 
-    let lower_confidence_interval = mean_ns - confidence_interval_margin;
-    let upper_confidence_interval = mean_ns + confidence_interval_margin;
-
-    println!(
-        "\nBENCHMARK RESULTS\n---{color_blue}Program{color_reset}---\n{color_blue}{}{color_reset}\n-------------",
-        src.1
-    );
-    println!("Samples          : {}", samples_count);
-    println!("Min              : {:.3} ms", min_ns as f64 / 1000000.0);
-    println!("Max              : {:.3} ms", max_ns as f64 / 1000000.0);
-    println!(
-        "Median           : {:.3} ms",
-        median_ns as f64 / 1_000_000.0
-    );
-    println!("Mean             : {:.3} ms", mean_ns / 1000000.0);
-    println!("Stand. deviation : {:.3} ms", std_deviation / 1000000.0);
-    println!(
-        "95% Confidence interval for mean  : [{:.3} .. {:.3}] ms",
-        lower_confidence_interval / 1000000.0,
-        upper_confidence_interval / 1000000.0
-    );
-    println!(
-        "{bg_red}{color_white}Average exec time: {:.3} ms (±{:.3} ms with 95% confidence){color_reset}{bg_reset}",
-        mean_ns / 1000000.0,
-        confidence_interval_margin / 1000000.0
-    );
+        println!(
+            "\nBENCHMARK RESULTS\n---{color_blue}Program{color_reset}---\n{color_blue}{}{color_reset}\n-------------",
+            src.1
+        );
+        println!("Samples          : {}", samples_count);
+        println!("Min              : {:.3} ms", min_ns as f64 / 1000000.0);
+        println!("Max              : {:.3} ms", max_ns as f64 / 1000000.0);
+        println!(
+            "Median           : {:.3} ms",
+            median_ns as f64 / 1_000_000.0
+        );
+        println!("Mean             : {:.3} ms", mean_ns / 1000000.0);
+        println!("Stand. deviation : {:.3} ms", std_deviation / 1000000.0);
+        println!(
+            "95% Confidence interval for mean  : [{:.3} .. {:.3}] ms",
+            lower_confidence_interval / 1000000.0,
+            upper_confidence_interval / 1000000.0
+        );
+        println!(
+            "{bg_red}{color_white}Average exec time: {:.3} ms (±{:.3} ms with 95% confidence){color_reset}{bg_reset}",
+            mean_ns / 1000000.0,
+            confidence_interval_margin / 1000000.0
+        );
+    }
 }
